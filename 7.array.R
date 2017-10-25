8fc5e599d70aa88ecc99b48776299d2cd5ec4e39
Donggoo Jung <dgjung0220@gmail.com>
2017-10-25 09:47:00 +0900

    7. array modified

--- 7.array.R
+++ 7.array.R
@@ -62,4 +62,26 @@ array(letters, dim=c(2,3,2,2))
 # [1,] "s"  "u"  "w" 
 # [2,] "t"  "v"  "x"
 
+### dimnames 활용
+v1 <- c("1행","2행")
+v2 <- c("1열", "2열", "3열")
+v3 <- c("1면", "2면")
+
+matrix(letters[1:6], nrow=2, ncol=3, dimnames = list(v1,v2))
+#     1열 2열 3열
+# 1행 "a" "c" "e"
+# 2행 "b" "d" "f"
+
+array(letters, dim=c(2,3,2), dimnames = list(v1,v2,v3))
+# , , 1면
+
+#     1열 2열 3열
+# 1행 "a" "c" "e"
+# 2행 "b" "d" "f"
+
+# , , 2면
+
+#     1열 2열 3열
+# 1행 "g" "i" "k"
+# 2행 "h" "j" "l"
 
